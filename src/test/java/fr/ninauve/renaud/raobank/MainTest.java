49f734a965c3e87c1d0f768414f0be51313597f8
rninauve <renaud.ninauve@chanel.com>
2021-05-24 14:47:04 +0200

    init

---
+++ src/test/java/fr/ninauve/renaud/raobank/MainTest.java
@@ -0,0 +1,14 @@
+package fr.ninauve.renaud.raobank;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MainTest {
+
+  @Test
+  public void test() {
+
+    assertThat(true).isTrue();
+  }
+}
