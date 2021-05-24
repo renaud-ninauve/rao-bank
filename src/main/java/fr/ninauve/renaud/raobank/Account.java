a4e5734649640b1edda710a284f6f2ae2fbb6b00
rninauve <renaud.ninauve@chanel.com>
2021-05-24 16:30:49 +0200

    add account

--- src/main/java/fr/ninauve/renaud/raobank/Account.java
+++ src/main/java/fr/ninauve/renaud/raobank/Account.java
@@ -2,4 +2,16 @@ package fr.ninauve.renaud.raobank;
 
 public class Account {
 
+  public void deposit(long amount) {
+    
+  }
+
+  public void retrieval(long amount) {
+
+  }
+
+  public long balance() {
+
+    return 0l;
+  }
 }
