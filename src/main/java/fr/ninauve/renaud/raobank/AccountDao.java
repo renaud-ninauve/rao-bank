f5775fc346ca62789c5944a8ce2e1621124b3814
rninauve <renaud.ninauve@chanel.com>
2021-05-24 16:53:20 +0200

    [green] account dao tests

--- src/main/java/fr/ninauve/renaud/raobank/AccountDao.java
+++ src/main/java/fr/ninauve/renaud/raobank/AccountDao.java
@@ -2,11 +2,15 @@ package fr.ninauve.renaud.raobank;
 
 public class AccountDao {
 
+  private Account account = new Account(1000L);
+
   public Account find(String userAccount) {
 
-    return null;
+    return account;
   }
 
   public void update(Account account) {
+
+    this.account = account;
   }
 }
