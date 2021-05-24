74092404982d966a281a64a48b9b5160114a15b6
rninauve <renaud.ninauve@chanel.com>
2021-05-24 14:53:49 +0200

    add spring

--- src/main/java/fr/ninauve/renaud/raobank/Main.java
+++ src/main/java/fr/ninauve/renaud/raobank/Main.java
@@ -1,8 +1,22 @@
 package fr.ninauve.renaud.raobank;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Main {
 
   public static void main(String... args) {
+
+    final AnnotationConfigApplicationContext applicationContext =
+        new AnnotationConfigApplicationContext(MainConfiguration.class);
+
+    final Main main = applicationContext.getBean(Main.class);
+    main.run();
+    applicationContext.close();
+  }
+
+  public void run() {
 
     System.out.println("hello");
   }
---
+++ src/main/java/fr/ninauve/renaud/raobank/MainConfiguration.java
@@ -0,0 +1,10 @@
+package fr.ninauve.renaud.raobank;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("fr.ninauve.renaud.raobank")
+public class MainConfiguration {
+
+}
