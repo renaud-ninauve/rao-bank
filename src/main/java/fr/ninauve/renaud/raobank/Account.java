735a0fa4f25a3b3f22389072c198b1fc79a2eaff
rninauve <renaud.ninauve@chanel.com>
2021-05-24 16:41:52 +0200

    account tests

--- src/main/java/fr/ninauve/renaud/raobank/Account.java
+++ src/main/java/fr/ninauve/renaud/raobank/Account.java
@@ -2,8 +2,14 @@ package fr.ninauve.renaud.raobank;
 
 public class Account {
 
+  private long balance;
+
+  public Account(long balance) {
+    this.balance = balance;
+  }
+
   public void deposit(long amount) {
-    
+
   }
 
   public void retrieval(long amount) {
---
+++ src/test/java/fr/ninauve/renaud/raobank/AccountTest.java
@@ -0,0 +1,50 @@
+package fr.ninauve.renaud.raobank;
+
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class AccountTest {
+
+  private Account account;
+
+  private static final long BALANCE_INIT = 1000L;
+  private static final long AMOUNT = 12L;
+  private static final long BALANCE_AFTER_DEPOSIT = 1012L;
+  private static final long BALANCE_AFTER_RETRIEVE = 988L;
+
+  @Before
+  public void setUp() {
+
+    this.account = new Account(BALANCE_INIT);
+  }
+
+  @Test
+  public void should_consult() {
+
+    final long actual = account.balance();
+
+    assertThat(actual).isEqualTo(BALANCE_INIT);
+  }
+
+  @Test
+  public void should_deposit() {
+
+    account.deposit(AMOUNT);
+
+    final long actual = account.balance();
+
+    assertThat(actual).isEqualTo(BALANCE_AFTER_DEPOSIT);
+  }
+
+  @Test
+  public void should_retrieve() {
+
+    account.retrieval(AMOUNT);
+
+    final long actual = account.balance();
+
+    assertThat(actual).isEqualTo(BALANCE_AFTER_RETRIEVE);
+  }
+}
